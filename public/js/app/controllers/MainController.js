4f3e8916cbf6cebc4994c93578da763fa15efe5b
Clayton Fidelis <clayton.fidelis@live.com>
2016-04-24 14:46:32 -0300

    Interface

--- public/js/app/controllers/MainController.js
+++ public/js/app/controllers/MainController.js
@@ -14,7 +14,8 @@
 		function artistToNode(artist){
     		var node = {
     			id: artist.id,
-    			label: artist.name
+    			label: artist.name,
+    			value: artist.followers.total
     		};
 
     		if($scope.showImages){
@@ -47,6 +48,9 @@
 		$scope.playTopTrack = function(id){
 			SpotifyService.searchTopTracks(id)
 			.then(function(res){
+				$scope.track = res.data.tracks[0];
+				$scope.tracks = res.data.tracks;
+
 				audioObject.pause();
 				audioObject = new Audio(res.data.tracks[0].preview_url);
                 audioObject.play();
@@ -101,15 +105,15 @@
 	    	similar.forEach(function(artist, index){
     			var edge = {from: id, to: artist.id};
     			var node = artistToNode(artist);
-    			if(index < 3){
+    			
+    			if(index < 4){
 	    			try{	    		
-
 	    				nodesArray.push(node);
-	    				nodes.add(node);
 
+	    				nodes.add(node);
 		    			if(!existsEdge(edge) && existsNode(node)){
 		    				edgesArray.push(edge);
-		    				edges.add(edge);	
+		    				edges.add(edge);
 		    			}
 		    		} catch(err){
 		    			if($scope.layout !== 'hierarchical'){
@@ -147,6 +151,8 @@
 	    function defaultOptions(){
 	    	return {
 			  	nodes: {
+		  			borderWidth: 3,
+    				borderWidthSelected: 3,
 			    	shape: 'dot',
 			    	scaling: {
 			    		min: 20,
@@ -160,14 +166,42 @@
 			    	font: {
 		    			size: 12, 
 		    			face: 'Arial'
-		    		}
+		    		},
+		    		borderWidth: 3,
+				    color: {
+				      	border: "#1ed760",
+				      	background: "#0a0a0a",
+				      	highlight: {
+				        	border: "#D3D71E",
+				        	background: "#0a0a0a"
+				      	},
+				      	hover: {
+        					border: "#fff",
+        					background: "#0a0a0a"
+      					}
+				    },
 			  	},
 			  	interaction: {
 				    hover: true,
 				    hoverConnectedEdges: true,
 				    selectConnectedEdges: true,
 			  	},
+				edges: {
+				    color: {
+				      	color: "rgba(31,33,32,1)",
+				      	highlight: "#1ed760",
+				      	inherit: false
+				    }
+			  	},
 			};
+	    }
+
+	    $scope.playTrack = function(track){
+	    	$scope.track = track;
+	    	audioObject.pause();
+	    	audioObject = new Audio(track.preview_url);
+            audioObject.play();
+            console.log(track);
 	    }
 	});
 })();
