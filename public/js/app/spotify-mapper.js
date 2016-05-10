36a80e9ae4bb02d924a03f8505ee50e77cc2698c
Clayton Fidelis <clayton.fidelis@live.com>
2016-05-09 22:38:07 -0300

    Bug fix: Redirect URI

--- public/js/app/spotify-mapper.js
+++ public/js/app/spotify-mapper.js
@@ -12,7 +12,7 @@
 
     	cfpLoadingBarProvider.includeSpinner = false;
     	SpotifyProvider.setClientId('8bca6dd295154108baa9485fddd1e4f1');
-    	SpotifyProvider.setRedirectUri('http://spotify-mapper.herokuapp.com/callback.html');
+    	SpotifyProvider.setRedirectUri('https://spotify-mapper.herokuapp.com/callback.html');
     	// SpotifyProvider.setRedirectUri('http://localhost:3000/callback.html');
     	SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public');
     	if($cookies.get('token') != undefined)
