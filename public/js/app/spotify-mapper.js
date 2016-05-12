4ca4797a8a25220bb99be8fa75218935a3979e14
Clayton Fidelis <clayton.fidelis@live.com>
2016-05-11 22:51:19 -0300

    Changing auth url

--- public/js/app/spotify-mapper.js
+++ public/js/app/spotify-mapper.js
@@ -12,8 +12,8 @@
 
         cfpLoadingBarProvider.includeSpinner = false;
         SpotifyProvider.setClientId('8bca6dd295154108baa9485fddd1e4f1');
-        // SpotifyProvider.setRedirectUri('https://spotify-mapper.herokuapp.com/callback.html');
-        SpotifyProvider.setRedirectUri('http://localhost:3000/callback.html');
+        SpotifyProvider.setRedirectUri('https://spotify-mapper.herokuapp.com/callback.html');
+        // SpotifyProvider.setRedirectUri('http://localhost:3000/callback.html');
         SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public');
         if($cookies.get('token') != undefined)
             SpotifyProvider.setAuthToken($cookies.get('token'));
